71035de465f0d77999c52ea5778af35c08bd567f
qxc <qxc@meitu.com>
2019-07-18 10:06:00 +0800

    add models from DIP

--- train.py
+++ train.py
@@ -3,7 +3,8 @@ import cv2
 from models import UNet
 import numpy as np
 import utils
-def train(net, input_noise, img, mask, optimizer, iter_num, is_gpu = True):
+from models_from_dip import skip
+def train(net, input_noise, img, mask, optimizer, iter_num, is_gpu = False):
     if is_gpu:
         net = net.cuda()
         input_noise = input_noise.cuda()
@@ -21,8 +22,8 @@ def train(net, input_noise, img, mask, optimizer, iter_num, is_gpu = True):
 
         #imshow
         output_numpy = utils.tensor_to_numpy(output).astype(np.uint8)
-        #cv2.imshow('output', output_numpy)
-        #cv2.waitKey(1000)
+        cv2.imshow('output', output_numpy)
+        cv2.waitKey(100)
         cv2.imwrite('output.png', output_numpy)
         #histgram
         img_numpy = utils.tensor_to_numpy(img).astype(np.uint8)
@@ -43,9 +44,17 @@ def main():
     img = torch.unsqueeze(img,0)
     img = img / 255.0
     #read net
-    net = UNet()
+    #net = UNet()
+    pad = 'reflection'
+    net = skip(1, 1,
+               num_channels_down=[128] * 5,
+               num_channels_up=[128] * 5,
+               num_channels_skip=[128] * 5,
+               filter_size_up=3, filter_size_down=3,
+               upsample_mode='nearest', filter_skip_size=1,
+               need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU')
     #get optimizer
-    optimizer = torch.optim.Adam(net.parameters(), lr = 0.0001)
+    optimizer = torch.optim.Adam(net.parameters(), lr = 0.001)
     #train parameter
     iter_num = 10000
     mask_size = (1,1,512,512)
